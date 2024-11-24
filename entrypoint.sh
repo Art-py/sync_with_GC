fa2797b25ac3ab80c5ab4e7b63fd3f4e17174aef
av.zhukov1 <av.zhukov1@tensor.ru>
2024-11-24 15:39:00 +0300

    add create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,23 @@ fi
 echo "Running migrations..."
 python sync_with_GC/manage.py migrate
 
+# Создание суперпользователя
+echo "Creating superuser if not exists..."
+python sync_with_GC/manage.py shell << EOF
+from django.contrib.auth.models import User
+import os
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
+
 # Запускаем сервер разработки
 echo "Starting server..."
 python sync_with_GC/manage.py runserver 0.0.0.0:8000
